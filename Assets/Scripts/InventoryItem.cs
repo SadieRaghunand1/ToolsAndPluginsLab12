dfe2b4c282ebbd0407729e1400f7c5b1c8e17f8b
Makayla-ALD <ma654968@ucf.edu>
2025-11-09 14:50:44 -0500

    new unity editor

---
+++ Assets/Scripts/InventoryItem.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class InventoryItem
+{
+    int id;
+    string itemName;
+    float value;
+
+
+    public void InitializeItem(string _name)
+    {
+        id = Random.Range(0, 100);
+        itemName = _name;
+        value = Random.Range(0f, 100f);
+    }
+
+    public string ReturnName()
+    {
+        return itemName;
+    }
+
+    public string ReturnValue()
+    {
+        return value;
+    }
+}
---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    [SerializeField] private List<InventoryItem> items;
+    [SerializeField] List<string> possibleNames;
+
+    private void Start()
+    {
+        InitializeInventory();
+        QuickSortByValue();
+        LinearSearchByName("Quen");
+    }
+
+    void InitializeInventory()
+    {
+        items = new List<InventoryItem>();  
+
+        int _size = Random.Range(3, 20);
+        for (int i = 0; i < _size; i++)
+        {
+            //Create item
+            InventoryItem _newItem = new InventoryItem();
+
+            //Randomize name
+            int _i = Random.Range(0, possibleNames.Count);
+            string _name = possibleNames[_i];
+            possibleNames.RemoveAt(_i);
+
+            _newItem.InitializeItem(_name);
+            items.Add(_newItem);
+        }
+
+    }
+
+    InventoryItem LinearSearchByName(string itemName)
+    {
+        InventoryItem _itemFound = null;
+
+        for(int i = 0; i < items.Count; i++)
+        {
+            if (items[i].ReturnName() == itemName)
+            {
+                _itemFound = items[i];
+                Debug.Log(itemName);
+                return _itemFound;
+            }
+        }
+        Debug.Log("No item found");
+        return null;
+    }
+
+    public void QuickSortByValue()
+    {
+        if (items == null || items.Count == 0)
+            return;
+        
+        QuickSort(0, items.Count - 1);
+
+        for (int i = 0; i < items.Count; i++)
+        {
+            Debug.Log($"[{i}] {items[i].ReturnValue()}");
+        }
+    }
+
+    public void QuickSort(int low, int high)
+    {
+        if (low < high)
+        {
+            int pivot = Partition(low, high);
+            QuickSort(low, pivot - 1);
+            QuickSort(pivot + 1, high);
+        }
+    }
+
+    public int Partition(int low, int high)
+    {
+        float pivot = items[high].ReturnValue();
+        int i = low - 1;
+
+        for (int j = low; j <= high; j++)
+        {
+            if (items[j].ReturnValue() <= pivot)
+            {
+                i++;
+
+                InventoryItem temporary = items[low];
+                items[low] = items[high];
+                items[high] = temporary;
+
+            }
+        }
+        InventoryItem temporaryNext = items[low + 1];
+        items[low + 1] = items[high];
+        items[high] = temporaryNext;
+
+        return i + 1;
+    }
+
+
+}
